d742ecfe495460ad92f6332d2c805ebb9321bfb7
Jonathan Lee <jonathanlee882@gmail.com>
2023-07-07 00:44:29 +1000

    added function and syntax code

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+// basic tyoes
+let age = 25;
+let myArray = ["one", "two", "three"]; // Array of strings
+let isAdult = true;
+let fruits = ["apple", "orange"];
+let variables = ["apple", "orange"];
+let numArr = [[1], [2, 3]];
+// Tuples
+let employee = [1, "jack"];
+let employees = [
+    [2, "1"]
+];
+let newCar = {
+    brand: "Lexus",
+    year: 2020
+};
+// customy typer
+// Union
+let x = 22;
+x = "hello";
+let jOccupation = "employed";
+// Type assetion
+let y = 2;
+y = ;
+"hello";
---
+++ index.ts
@@ -0,0 +1,50 @@
+// basic tyoes
+let age: number = 25;
+let myArray: Array<string> = ["one", "two", "three"]; // Array of strings
+let isAdult: boolean = true;    
+
+let fruits: Array<string> = ["apple", "orange"];
+let variables: Array<any> = ["apple", "orange"];
+
+let numArr: number[][] = [[1], [2,3]]
+
+// Tuples
+let employee: [number, string] = [1, "jack"];
+let employees : [number, string][] = [
+    [2, "1"]
+]
+
+// objects
+
+type Car = {
+    brand: string,
+    year: number
+}
+
+// let car1 : Car = {
+//     brand: "Lexus",
+//     year: 2020
+// }
+
+interface Car1 {
+    brand: string, 
+    year: number
+}
+
+let newCar : Car1 = {
+    brand: "Lexus",
+    year: 2020
+}
+// customy typer
+
+// Union
+let x : string  | number = 22;
+x = "hello"
+
+type occupation = "employed" | "student"
+
+let jOccupation : occupation = "employed"
+
+// Type assetion
+let y: number = 2
+y = <string> = "hello"
